b2091c5c714c1fc4d2f4a477c45a846d48e29426
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-06-05 09:21:48 -0600

    Test: initialize w/ attributes

--- spec/rides_spec.rb
+++ spec/rides_spec.rb
@@ -1,3 +1,18 @@
 require './lib/rides'
 require 'rspec'
 
+RSpec.describe "Rides" do 
+  describe "#initialize" do 
+    it "ride1 can initialize with attributes" do 
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+
+      expect(ride1).to be_a(Ride)
+      expect(ride1.name).to eq("Walnut Creek Trail")
+      expect(ride1.distance).to eq(10.7)
+      expect(ride1.terrain).to eq(:hills)
+      expect(ride1.loop?).to eq(false)
+      # expect(ride1.total_distance).to eq(21.4)
+    end
+
+  end
+end
