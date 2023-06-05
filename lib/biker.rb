4ceb298f96f9c6a8d814a550639d581f6877a6c8
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-06-05 11:54:58 -0600

    Feat: incomplete log_ride method

--- lib/biker.rb
+++ lib/biker.rb
@@ -14,4 +14,13 @@ attr_reader :name, :max_distance, :rides,
     @acceptable_terrain << new_terrain
   end
 
+  #This method was a doozy. I could not figure out how to NOT overwrite my array.
+  #I will be focusing on this with student support today, so any notes are greatly appreciated. 
+
+  def log_ride(ride, time)
+   # # return "cannot complete ride" if !distance < biker.max_distance && @terrain.include?(@acceptable_terrain)
+      @rides[ride] = []
+      @rides[ride] = [] << time
+  end
+  
 end
